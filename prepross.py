70eb9294d40a4e9a59f27199450c0ea663988d27
DaronP <jandresperez1@yahoo.com>
2022-02-25 19:44:40 -0600

    Features actualizados, modelo 1 aun no creado

--- prepross.py
+++ prepross.py
@@ -39,7 +39,9 @@ descartar = ['biflow_direction',
 'dst_tos',
 'sampling_interval',
 'protocol',
-'l4_dst_port']
+'l4_dst_port',
+'IPV4_DST_ADDR',
+'IPV4_SRC_ADDR']
 
 for i in range(len(descartar)):
     descartar[i] = descartar[i].upper()
@@ -49,6 +51,8 @@ descartar.append('Unnamed: 0.1')
 
 df = pd.read_csv('dataframe_spl.csv')
 
+print(df['L7_PROTO_NAME'].unique())
+
 '''corr_df = df.corr(method='pearson')
 print(corr_df)
 sb.heatmap(corr_df, annot=True)
@@ -57,42 +61,66 @@ plt.show()'''
 #print(df.head())
 df = df.drop(descartar, axis=1)
 
+df.loc[df['LABEL'] == 'Normal flow', 'LABEL'] = 0
+df.loc[df['LABEL'] == 'SYN Scan - aggressive', 'LABEL'] = 1
+df.loc[df['LABEL'] == 'Denial of Service R-U-Dead-Yet', 'LABEL'] = 1
+df.loc[df['LABEL'] == 'NULL Scan', 'LABEL'] = 1
+df.loc[df['LABEL'] == 'UDP Scan', 'LABEL'] = 1
+
+df.loc[df['PROTOCOL_MAP'] == 'tcp', 'PROTOCOL_MAP'] = 1
+df.loc[df['PROTOCOL_MAP'] == 'udp', 'PROTOCOL_MAP'] = 2
+df.loc[df['PROTOCOL_MAP'] == 'icmp', 'PROTOCOL_MAP'] = 3
+df.loc[df['PROTOCOL_MAP'] == 'gre', 'PROTOCOL_MAP'] = 4
+
+print(df['PROTOCOL_MAP'].unique())
+
 byts = df['DST_TO_SRC_SECOND_BYTES'].to_list()
+byts2 = df['SRC_TO_DST_SECOND_BYTES'].to_list()
 
-for i in range(len(byts)):
-    if ',' in byts[i]:
-        
-        if len(byts[i]) == 1:
-
-            byts[i] = byts[i].replace(',', '0')
-        
-        if len(byts[i]) == 2 or len(byts[i]) == 3:
-            byts[i] = byts[i].replace(',', '')
-        
-        if len(byts[i]) > 3:
-            s = ''
-
-            for j in byts[i]:
-                if j != ',':
-                    s += j
-                
-                if s != '' and j == ',':
-                    byts[i] = s
-                    break
-
+def prep_bytes(byts):
+    for i in range(len(byts)):
         if ',' in byts[i]:
-            byts[i] = byts[i].strip(',')
-        if len(byts[i]) == 0:
-            byts[i] += '0'
+            
+            if len(byts[i]) == 1:
+
+                byts[i] = byts[i].replace(',', '0')
+            
+            if len(byts[i]) == 2 or len(byts[i]) == 3:
+                byts[i] = byts[i].replace(',', '')
+            
+            if len(byts[i]) > 3:
+                s = ''
+
+                for j in byts[i]:
+                    if j != ',':
+                        s += j
+                    
+                    if s != '' and j == ',':
+                        byts[i] = s
+                        break
+
+            if ',' in byts[i]:
+                byts[i] = byts[i].strip(',')
+            if len(byts[i]) == 0:
+                byts[i] += '0'
+    
+    return byts
+
+byts = prep_bytes(byts)
+byts2 = prep_bytes(byts2)
             
 for i in range(len(byts)):
     byts[i] = int(byts[i])
+
+for i in range(len(byts2)):
+    byts2[i] = int(byts2[i])
 
 
 
 
 
 df['DST_TO_SRC_SECOND_BYTES'] = byts
+df['SRC_TO_DST_SECOND_BYTES'] = byts2
 
 
 corr_df = df.corr(method='pearson')
--- training.py
+++ training.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np
 import pickle
 from gensim.parsing.preprocessing import remove_stopwords
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics, model_selection
 
 
 df = pd.read_csv('df_prep.csv')
-
+print(df['LABEL'])
 
 target = df['LABEL']
 
@@ -19,4 +19,15 @@ final_features = features.columns
 
 print('Final features: ', final_features)
 
-featur
+feature_matrix_train, feature_matrix_test, target_train, target_test = model_selection.train_test_split(features, target, test_size=0.30, random_state=32)
+
+
+clf = MultinomialNB()
+clf = clf.fit(feature_matrix_train, target_train)
+
+target_pred = clf.predict(feature_matrix_test)
+
+
+print(metrics.accuracy_score(target_test, target_pred))
+print('Matriz de confusion /n',metrics.confusion_matrix(target_test, target_pred))
+print(metrics.classification_report(target_test, target_pred, target_names=['spam', 'legitimate']))
